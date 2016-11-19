064ae5ae14a6686f30263486a03afe25bf48afa6
mazen17 <mazen17>
2016-11-19 22:35:01 +0300

    configurations

--- src/main/java/com/github/elizabetht/repository/StudentRepository.java
+++ src/main/java/com/github/elizabetht/repository/StudentRepository.java
@@ -1,16 +1,14 @@
 package com.github.elizabetht.repository;
 
+import com.github.elizabetht.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.github.elizabetht.model.Student;
-
-@Repository("studentRepository")
+@Repository("employeeRepository")
 public interface StudentRepository extends JpaRepository<Student, Long> {
-	
-	@Query("select s from Student s where s.userName = :userName")
-	Student findByUserName(@Param("userName") String userName);
-	
+  @Query("select s from Student s where s.userName = :userName")
+  Student findByUserName(@Param("userName") String userName);
+
 }
